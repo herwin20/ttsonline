382d8fca43e68079d53a8996f4dd2993b2ab74bf
Herwin Januardi <herwinjanuardi20@gmail.com>
2024-04-18 22:37:01 +0700

    Create question_lists.js
    Add javascript

---
+++ question_lists.js
@@ -0,0 +1,18 @@
+var quiz = [
+  [1, 5, "Apa Yang Dimaksud Dengan Mamalia", "MAMANYALIA", "datar"],
+  /*[
+    0,
+    8,
+    "Apa Yang Dilakukan Ibu-Ibu Ketika Malam Hari Bersama Suaminya",
+    "TIDUR",
+    "turun",
+  ], */
+  [0,5, "Ada Raja, Ada Ratu, Ada Pangeran, Jika Rajanya Meninggal Pangeran Jadi", "YATIM", "turun"],
+  /*[4, 6, "Ada apa di ujung Laut", "HURUFT", "datar"],
+  [3, 0, "Ani menggambar segitiga sama", "KIKI", "datar"],
+  [2, 11, "Harimau mati meninggal kan", "SETUJU", "turun"],
+  [4, 6, "Yang muncul kalau hujan", "HUJAN", "turun"],
+  [7, 3, "Makhluk hidup pemburu WIFI", "ANDA", "datar"],
+  [6, 3, "Salah satu Bahasa Pemrograman berorientasi objek", "JAVA", "turun"],
+  [2, 11, "Enam (bahasa Inggris", "SIX", "datar"], */
+];
